ff1c7bf4ea8fe36992f01fccf51a1b9443aa324d
adeeshbhargava <adeeshb@andrew.cmu.edu>
2023-05-10 10:10:43 -0400

    Adding PPO for mountain car , cartpole and pendulum

--- RL/Safe_RL_baselines/PPO-Lagrangian/PPO_cartpole.py
+++ RL/Safe_RL_baselines/PPO-Lagrangian/PPO_cartpole.py
@@ -17,25 +17,23 @@ from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 from tensorboardX import SummaryWriter
 
 # Parameters
-env_name = 'MountainCar-v0'
 gamma = 0.99
 render = False
 seed = 1
 log_interval = 10
 
-env = gym.make(env_name).unwrapped
+env = gym.make('CartPole-v0').unwrapped
 num_state = env.observation_space.shape[0]
 num_action = env.action_space.n
 torch.manual_seed(seed)
 env.seed(seed)
-Transition = namedtuple('Transition', ['state', 'action', 'a_log_prob', 'reward', 'next_state'])
-
+Transition = namedtuple('Transition', ['state', 'action',  'a_log_prob', 'reward', 'next_state'])
 
 class Actor(nn.Module):
     def __init__(self):
         super(Actor, self).__init__()
-        self.fc1 = nn.Linear(num_state, 128)
-        self.action_head = nn.Linear(128, num_action)
+        self.fc1 = nn.Linear(num_state, 100)
+        self.action_head = nn.Linear(100, num_action)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -46,8 +44,8 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     def __init__(self):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(num_state, 128)
-        self.state_value = nn.Linear(128, 1)
+        self.fc1 = nn.Linear(num_state, 100)
+        self.state_value = nn.Linear(100, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -59,7 +57,7 @@ class PPO():
     clip_param = 0.2
     max_grad_norm = 0.5
     ppo_update_time = 10
-    buffer_capacity = 8000
+    buffer_capacity = 1000
     batch_size = 32
 
     def __init__(self):
@@ -83,7 +81,7 @@ class PPO():
             action_prob = self.actor_net(state)
         c = Categorical(action_prob)
         action = c.sample()
-        return action.item(), action_prob[:, action.item()].item()
+        return action.item(), action_prob[:,action.item()].item()
 
     def get_value(self, state):
         state = torch.from_numpy(state)
@@ -99,13 +97,14 @@ class PPO():
         self.buffer.append(transition)
         self.counter += 1
 
+
     def update(self, i_ep):
         state = torch.tensor([t.state for t in self.buffer], dtype=torch.float)
         action = torch.tensor([t.action for t in self.buffer], dtype=torch.long).view(-1, 1)
         reward = [t.reward for t in self.buffer]
         # update: don't need next_state
-        # reward = torch.tensor([t.reward for t in self.buffer], dtype=torch.float).view(-1, 1)
-        # next_state = torch.tensor([t.next_state for t in self.buffer], dtype=torch.float)
+        #reward = torch.tensor([t.reward for t in self.buffer], dtype=torch.float).view(-1, 1)
+        #next_state = torch.tensor([t.next_state for t in self.buffer], dtype=torch.float)
         old_action_log_prob = torch.tensor([t.a_log_prob for t in self.buffer], dtype=torch.float).view(-1, 1)
 
         R = 0
@@ -114,20 +113,20 @@ class PPO():
             R = r + gamma * R
             Gt.insert(0, R)
         Gt = torch.tensor(Gt, dtype=torch.float)
-        # print("The agent is updateing....")
+        #print("The agent is updateing....")
         for i in range(self.ppo_update_time):
             for index in BatchSampler(SubsetRandomSampler(range(len(self.buffer))), self.batch_size, False):
-                if self.training_step % 1000 == 0:
-                    print('I_ep {} ，train {} times'.format(i_ep, self.training_step))
-                # with torch.no_grad():
+                if self.training_step % 1000 ==0:
+                    print('I_ep {} ，train {} times'.format(i_ep,self.training_step))
+                #with torch.no_grad():
                 Gt_index = Gt[index].view(-1, 1)
                 V = self.critic_net(state[index])
                 delta = Gt_index - V
                 advantage = delta.detach()
                 # epoch iteration, PPO core!!!
-                action_prob = self.actor_net(state[index]).gather(1, action[index])  # new policy
+                action_prob = self.actor_net(state[index]).gather(1, action[index]) # new policy
 
-                ratio = (action_prob / old_action_log_prob[index])
+                ratio = (action_prob/old_action_log_prob[index])
                 surr1 = ratio * advantage
                 surr2 = torch.clamp(ratio, 1 - self.clip_param, 1 + self.clip_param) * advantage
 
@@ -139,7 +138,7 @@ class PPO():
                 nn.utils.clip_grad_norm_(self.actor_net.parameters(), self.max_grad_norm)
                 self.actor_optimizer.step()
 
-                # update critic network
+                #update critic network
                 value_loss = F.mse_loss(Gt_index, V)
                 self.writer.add_scalar('loss/value_loss', value_loss, global_step=self.training_step)
                 self.critic_net_optimizer.zero_grad()
@@ -148,9 +147,9 @@ class PPO():
                 self.critic_net_optimizer.step()
                 self.training_step += 1
 
-        del self.buffer[:]  # clear experience
+        del self.buffer[:] # clear experience
 
-
+    
 def main():
     agent = PPO()
     for i_epoch in range(1000):
@@ -165,11 +164,10 @@ def main():
             agent.store_transition(trans)
             state = next_state
 
-            if done:
-                if len(agent.buffer) >= agent.batch_size: agent.update(i_epoch)
-                agent.writer.add_scalar('Steptime/steptime', t, global_step=i_epoch)
+            if done :
+                if len(agent.buffer) >= agent.batch_size:agent.update(i_epoch)
+                agent.writer.add_scalar('liveTime/livestep', t, global_step=i_epoch)
                 break
-
 
 if __name__ == '__main__':
     main()
